7cf642bda7cef4a216f63729b305ad5be80cc905
JustinEBlake <116922273+JustinEBlake@users.noreply.github.com>
2024-03-21 17:50:33 -0500

    delete unused code

--- app.py
+++ app.py
@@ -12,24 +12,17 @@ from sklearn.preprocessing import StandardScaler, OneHotEncoder
 import joblib
 from joblib import dump, load
 import sqlite3
-
-
-# Database setup
-# db = Path("Resources/credit_risk.sqlite")
-# def get_db_connection(path):
-#     conn = sqlite3.connect(path)
-#     conn.row_factory = sqlite3.Row
-#     return conn
+import pandas as pd
 
 
 app = Flask(__name__)
 CORS(app)
 
-# # Load tensorflow model
-# model = tf.keras.models.load_model('Resources/tensorflowmodel.keras')
+# Load tensorflow model
+model = tf.keras.models.load_model('Resources/tensorflowmodel.keras')
 
 # encoder = joblib.load('Resources/encoder.joblib')
-# scaler = joblib.load('Resources/scaler.joblib')
+scaler = joblib.load('Resources/scaler.joblib')
 
 @app.route('/')
 def home():
@@ -38,66 +31,72 @@ def home():
 
 @app.route('/evaluate-risk', methods=['POST'])
 def evaluate_risk():
-    # Check if JSON data is present in the request
-    if not request.json:
-        return jsonify({'error': 'No JSON data received'}), 400
+    data = request.json
+    # Preprocess data to match the model's training format
+    processed_data = process_(data)
 
-    # Get the JSON data from the request
-    data_received = request.json
+    # Predict using the TensorFlow model
+    prediction = model.predict(processed_data)
 
-    # Print the received data for debugging
-    print(data_received)
+    # Empty Variable to store response
+    answer = ''
 
-    # Return the received data as the response
-    return jsonify(data_received)
+    # Interpret the result
+    if prediction[0].tolist()[0] > 0.5:
+        answer = 'High Credit Risk'
+    else:
+        answer = "Low Credit Risk" 
 
-
-
-# @app.route('/evaluate-risk', methods=['POST'])
-# def evaluate_risk():
-#     data = request.json
-#     # Preprocess data to match the model's training format
-#     processed_data = preprocess(data)
-
-#     # Predict using the TensorFlow model
-#     prediction = model.predict(processed_data)
-
-#     # Interpret the result
-#     isCreditRisk = prediction[0] > 0.5 
-
-#     return jsonify({'isCreditRisk': isCreditRisk})
+    return jsonify({'Type': answer})
 
 
-# def preprocess(data):
-#     # Updated lists without 'loan_status'
-#     numerical_feature_names = ['person_age', 'person_income', 'person_emp_length', 'loan_amnt', 
-#                                'loan_percent_income', 'cb_person_cred_hist_length']
-#     categorical_feature_names = ['person_home_ownership_MORTGAGE', 'person_home_ownership_OTHER', 
-#                                  'person_home_ownership_OWN', 'person_home_ownership_RENT', 
-#                                  'loan_intent_DEBTCONSOLIDATION', 'loan_intent_EDUCATION', 
-#                                  'loan_intent_HOMEIMPROVEMENT', 'loan_intent_MEDICAL', 
-#                                  'loan_intent_PERSONAL', 'loan_intent_VENTURE', 'loan_grade_A', 
-#                                  'loan_grade_B', 'loan_grade_C', 'loan_grade_D', 'loan_grade_E', 
-#                                  'loan_grade_F', 'loan_grade_G', 'cb_person_default_on_file_N', 
-#                                  'cb_person_default_on_file_Y']
+def process_(data):
+    enc = OneHotEncoder(sparse_output=False)
     
-#     # Handling missing features by setting a default value or skipping
-#     numerical_features = np.array([data.get(feature, 0.0) for feature in numerical_feature_names]).reshape(1, -1)
-#     # Ensure categorical features are correctly extracted
-#     categorical_features = np.array([data.get(feature, 0) for feature in categorical_feature_names]).reshape(1, -1)
-    
-#     # Apply one-hot encoding to categorical features
-#     encoded_features = encoder.transform(categorical_features).toarray()
-    
-#     # Scale numerical features
-#     scaled_numerical_features = scaler.transform(numerical_features)
-    
-#     # Concatenate scaled numerical features and encoded categorical features
-#     processed_data = np.concatenate([scaled_numerical_features, encoded_features], axis=1)
-    
-#     return processed_data
+    # Convert our JSON Data to Pandas Dataframe
+    df = pd.DataFrame([data])
 
+    # Store our categorical values (Will use for encoder)
+    df_categ = df.dtypes[df.dtypes == 'object'].index.to_list()
 
+    # Fit and transform the OneHotEncoder using the categorical variable list
+    encode_df = pd.DataFrame(enc.fit_transform(df[df_categ]))
+
+    # Add the encoded variable names to the dataframe
+    encode_df.columns = enc.get_feature_names_out(df_categ)
+
+    # Merge the encoded categorical columns with our original colums
+    df = df.merge(encode_df, left_index=True, right_index=True)
+
+    # Drop the categorical colums that are not encoded
+    df = df.drop(df_categ, 1)
+
+    # Ensure all columns present in the training data are in the new data
+    training_categorical_columns = ['person_home_ownership_MORTGAGE', 'person_home_ownership_OTHER', 
+                                 'person_home_ownership_OWN', 'person_home_ownership_RENT', 
+                                 'loan_intent_DEBTCONSOLIDATION', 'loan_intent_EDUCATION', 
+                                 'loan_intent_HOMEIMPROVEMENT', 'loan_intent_MEDICAL', 
+                                 'loan_intent_PERSONAL', 'loan_intent_VENTURE', 'loan_grade_A', 
+                                 'loan_grade_B', 'loan_grade_C', 'loan_grade_D', 'loan_grade_E', 
+                                 'loan_grade_F', 'loan_grade_G', 'cb_person_default_on_file_N', 
+                                 'cb_person_default_on_file_Y']
+
+    missing_cols = set(training_categorical_columns).difference(df.columns)
+    for col in missing_cols:
+        df[col] = 0.0
+
+    # Reorder the columns to match the order in the training data
+    final_df = df[['person_age', 'person_income', 'person_emp_length', 
+                                                'loan_amnt', 'loan_int_rate','loan_percent_income', 'cb_person_cred_hist_length'] + training_categorical_columns  ]
+
+    # Get our X values
+    X = final_df.values
+
+    # Transform our X values using our saved scaler from the training data
+    X_scaled = scaler.transform(X)
+
+    # Return processed data
+    return X_scaled
 
 
 if __name__ == '__main__':
