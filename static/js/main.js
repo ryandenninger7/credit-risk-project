9b993640f21c55a301dc87ab8aab2cad652cc3ec
JustinEBlake <116922273+JustinEBlake@users.noreply.github.com>
2024-03-25 18:54:25 -0500

    send results to db

--- static/js/main.js
+++ static/js/main.js
@@ -53,6 +53,18 @@ document.addEventListener('DOMContentLoaded', function() {
         cb_person_cred_hist_length: parseInt(creditHistoryLength)
       }
 
+      // Get save button to allow for new results to be saved again
+      var saveResultsButton = document.getElementById('saveResultsButton');
+
+      // Update button text to indicate success
+      saveResultsButton.textContent = 'Save Results';
+
+      // Allow hover
+      saveResultsButton.classList.remove('clicked');
+
+      // Disable the button to prevent multiple clicks
+      saveResultsButton.disabled = false;
+
 
       // Send the form data to the Flask backend using AJAX
 fetch('/evaluate-risk', {
@@ -69,13 +81,13 @@ fetch('/evaluate-risk', {
 
     // Update the result text and color based on the prediction
     var resultField = document.getElementById('result');
-    resultField.value = `${firstName} ${lastName} is a ${prediction}`;
+    resultField.value = `${firstName.toUpperCase()} ${lastName.toUpperCase()} IS A ${prediction}`;
 
     // Remove existing color classes
     resultField.classList.remove('low-risk', 'high-risk');
 
     // Dynamically add color class based on the prediction
-    if (prediction === 'High Credit Risk!') {
+    if (prediction === 'HIGH CREDIT RISK') {
       resultField.classList.add('high-risk');
     } else {
       resultField.classList.add('low-risk');
@@ -85,4 +97,85 @@ fetch('/evaluate-risk', {
     console.error('Error:', error);
   });
 });
+  // Save Results Button Functionality
+  // Get the Save Results button element
+  var saveResultsButton = document.getElementById('saveResultsButton');
+
+  // Add click event listener to the button
+  saveResultsButton.addEventListener('click', function() {
+    // Get the value of the required input fields
+    var results = document.getElementById('result').value;
+    var firstName = document.getElementById('first_name').value;
+    var lastName = document.getElementById('last_name').value;
+    var age = document.getElementById('person_age').value;
+    var income = document.getElementById('person_income').value;
+    var loanIntent = document.getElementById('loan_intent').value;
+    var loanGrade = document.getElementById('loan_grade').value;
+    var loanAmount = document.getElementById('loan_amnt').value;
+
+    // Split results to array
+    var words = results.split(" ");
+
+    //Get last 3 words
+    var lastWords = words.slice(-3);
+
+    // Join words into str
+    var joinedResults = lastWords.join(" ");
+
+    // Date
+    var currentDate = new Date();
+
+    // Get the current year
+    var year = currentDate.getFullYear();
+
+    // Get the current month (0-11)
+    var month = currentDate.getMonth() + 1; // Adding 1 because months are zero-indexed
+
+    // Get the current day of the month (1-31)
+    var day = currentDate.getDate();
+
+    // Format the date as desired (e.g., DD-MM-YYYY)
+    var formattedDate = (month < 10 ? '0' + month : month) + '-' + (day < 10 ? '0' + day : day) + '-' + year;
+
+
+    // Dict of items to save to db
+    db_info = {
+      name: firstName + " " + lastName,
+      date: formattedDate,
+      income: income,
+      requestLoanAmount: loanAmount,
+      loanIntent: loanIntent,
+      loanGrade: loanGrade,
+      status: joinedResults
+    }
+
+    // Update button text to indicate success
+    saveResultsButton.textContent = 'Results Saved!';
+
+    // Disable the button to prevent multiple clicks
+    saveResultsButton.disabled = true;
+
+    // Add a class to the button to indicate that it has been clicked
+    saveResultsButton.classList.add('clicked');
+
+    // Log the results to console
+    console.log("Results saved: ", db_info);
+
+    // Send the results to a server 
+    fetch('/save_results', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: "application/json"
+      },
+      body: JSON.stringify(db_info)
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+    });
 });
